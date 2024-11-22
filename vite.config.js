2fa52dd001ff4641a78723a28f006eec701d4299
Eli <your.email@example.com>
2024-11-22 13:33:40 +0200

    fix: update configuration files for ES modules

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer
+      ]
+    }
+  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
     sourcemap: true
-  },
-  server: {
-    port: 5177
   }
 });
